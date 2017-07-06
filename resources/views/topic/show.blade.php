6acf2556866aba2603f4b40dec4ac3b4acbb4f86
Milos Jevtovic <milosjevtovicgm@gmail.com>
2017-07-06 21:45:24 +0200

    Updated topics

--- resources/views/topic/show.blade.php
+++ resources/views/topic/show.blade.php
@@ -14,7 +14,7 @@
 			@include('layouts.post')
 		@endforeach
 		@else
-			<h1> :( </h1> No posts are tagged with that.</p></div><h3 style="text-align:center;">That's all we've got on asdasdas.</h3><p style="text-align:center;">Pulsir is a hosted, beautiful and easy to use blogging platform. We take care of everything, no fees or ads on your content. <a href="/posts">Start writing today</a>, for free, forever.</p> </div>
+			<h1> :( </h1> No posts are tagged with that.</p></div><h3 style="text-align:center;">That's all we've got on {{ $topic }}.</h3><p style="text-align:center;">Pulsir is a hosted, beautiful and easy to use blogging platform. We take care of everything, no fees or ads on your content. <a href="/posts">Start writing today</a>, for free, forever.</p> </div>
 	@endif
 
 
